6b8afce1a11791abcf249834e5ca86db8df0eb3d
EC2 Default User <ec2-user@ip-172-31-14-52.us-east-2.compute.internal>
2025-01-31 22:54:08 +0000

    Added fixed welcomescript.sh to git-training

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-# Display a welcome message
-echo "Welcome to the Welcome Service!"
-
-# Display the hostname of the system
-echo "This script is running on $(hostname)"
-
-# Display the current date and time
-echo "Current date and time: $(date)"
-
-# Display the user running the script
-echo "The script is being executed by user: $USER"
-
-exit 0
+echo "Hello 312 Student!"
+echo "Enter Your Name Please:"
+read name
+echo "$name, welcome to 312 School"
 
